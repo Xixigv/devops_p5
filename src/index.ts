6b190a17edd0ae7363d0369c337fef727517a2d8
Ximena <ximena.villareal@iteso.mx>
2026-09-22 19:50:37 -0600

    conexion a base de datos

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,3 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export interface Env {
 	p6: D1Database;
 }
@@ -22,24 +9,28 @@ async function queryDatabase(db: D1Database) {
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		const url = new URL(request.url);
-
-		switch (url.pathname) {
-			case '/message':
-				return new Response('Hello, World!');
-			case '/random':
-				return new Response(crypto.randomUUID());
-			case '/health':
-				return Response.json({
-					status: 'ok',
-					timestamp: new Date().toISOString(),
-				});
-			case '/users': {
-				const data = await queryDatabase(env.p6);
-				return Response.json({ message: 'Hello, World! 3', dbData: data });
-			}
-			default:
-				return new Response('Not Found', { status: 404 });
-		}
+		const data = await queryDatabase(env.p6);
+		return Response.json({ message: 'Hello, World! 3', dbData: data });
 	},
+	// async fetch(request, env, ctx): Promise<Response> {
+	// 	const url = new URL(request.url);
+
+	// 	switch (url.pathname) {
+	// 		case '/message':
+	// 			return new Response('Hello, World!');
+	// 		case '/random':
+	// 			return new Response(crypto.randomUUID());
+	// 		case '/health':
+	// 			return Response.json({
+	// 				status: 'ok',
+	// 				timestamp: new Date().toISOString(),
+	// 			});
+	// 		case '/users': {
+	// 			const data = await queryDatabase(env.p6);
+	// 			return Response.json({ message: 'Hello, World! 3', dbData: data });
+	// 		}
+	// 		default:
+	// 			return new Response('Not Found', { status: 404 });
+	// 	}
+	// },
 } satisfies ExportedHandler<Env>;
