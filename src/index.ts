67f36a2d2b948781b833c422a2306b2039003c9e
Ximena <ximena.villareal@iteso.mx>
2026-09-22 19:39:19 -0600

    cambios con database funcional

--- src/index.ts
+++ src/index.ts
@@ -11,37 +11,35 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-
-export inerface Env {
+export interface Env {
 	p6: D1Database;
 }
 
+async function queryDatabase(db: D1Database) {
+	const { results } = await db.prepare('SELECT * FROM users').all();
+	return results;
+}
 
 export default {
-
 	async fetch(request, env, ctx): Promise<Response> {
-		// const url = new URL(request.url);
-		// switch (url.pathname) {
-		// 	case '/message':
-		// 		return new Response('Hello, World!');
-		// 	case '/random':
-		// 		return new Response(crypto.randomUUID());
-		// 	case '/health':
-		// 		return Response.json({
-		// 			status: 'ok',
-		// 			timestamp: new Date().toISOString(),
-		// 		});
-		// 	default:
-		// 		return new Response('Not Found', { status: 404 });
+		const url = new URL(request.url);
 
-		return Response.json({ message: 'Hello, World! 3', dbData: data });
+		switch (url.pathname) {
+			case '/message':
+				return new Response('Hello, World!');
+			case '/random':
+				return new Response(crypto.randomUUID());
+			case '/health':
+				return Response.json({
+					status: 'ok',
+					timestamp: new Date().toISOString(),
+				});
+			case '/users': {
+				const data = await queryDatabase(env.p6);
+				return Response.json({ message: 'Hello, World! 3', dbData: data });
+			}
+			default:
+				return new Response('Not Found', { status: 404 });
+		}
 	},
-
-	async queryDatabase(db: D1Database):  {
-		const { results } = await db.prepare('SELECT * FROM users').all();
-		return results;
-	}
-
-	
 } satisfies ExportedHandler<Env>;
-
